808f052711dc68a55d88223d733e7ed56a822145
Frommhund <56253401+Frommhund@users.noreply.github.com>
2022-01-31 20:43:45 +0100

    clean up compile

--- compile.ps1
+++ compile.ps1
@@ -1,1 +1,2 @@
 py -m nuitka --standalone --onefile --enable-plugin=anti-bloat --include-module=tqdm --windows-icon-from-ico=ns_icon_pink.ico NorthstarUpdater.py
+Remove-Item .\NorthstarUpdater.onefile-build\,.\NorthstarUpdater.dist\,.\NorthstarUpdater.build\ -Recurse
