8812ffe5ae07c2b56be0ab8060f88504a17b9932
Frommhund <56253401+Frommhund@users.noreply.github.com>
2022-02-06 17:38:25 +0100

    fixed self-replacer

--- NorthstarManager.py
+++ NorthstarManager.py
@@ -16,20 +16,13 @@ from tqdm import tqdm
 
 config = configparser.ConfigParser()
 g = Github()
+script_queue = []
 
 print(f"[{time.strftime('%H:%M:%S')}] [info]    Reading config from 'updater_config.ini'...")
 config.read("updater_config.ini")
 if len(config.sections()) == 0:
     print(f"[{time.strftime('%H:%M:%S')}] [warning] 'updater_config.ini' is empty or does not exist")
     print(f"[{time.strftime('%H:%M:%S')}] [info]    Using default config instead")
-    config["Northstar"] = {
-        "repository": "R2Northstar/Northstar",
-        "last_update": "0001-01-01T00:00:00",
-        "ignore_prerelease": "true",
-        "file": "NorthstarLauncher.exe",
-        "install_dir": ".",
-        "exclude_files": "ns_startup_args.txt|ns_startup_args_dedi.txt",
-    }
     config["NorthstarManager"] = {
         "repository": "FromWau/NorthstarManager",
         "last_update": "0001-01-01T00:00:00",
@@ -38,6 +31,14 @@ if len(config.sections()) == 0:
         "install_dir": ".",
         "exclude_files": "",
     }
+    config["Northstar"] = {
+        "repository": "R2Northstar/Northstar",
+        "last_update": "0001-01-01T00:00:00",
+        "ignore_prerelease": "true",
+        "file": "NorthstarLauncher.exe",
+        "install_dir": ".",
+        "exclude_files": "ns_startup_args.txt|ns_startup_args_dedi.txt",
+    }
     config["ExampleMod"] = {
         "repository": "example/example-mod",
         "last_update": "0001-01-01T00:00:00",
@@ -45,12 +46,6 @@ if len(config.sections()) == 0:
     config["Launcher"] = {
         "filename": "NorthstarLauncher.exe",
         "arguments": "-multiple",
-    }
-    config["SkinTool"] = {
-        "repository": "zxcPandora/Titanfall2-SkinTool",
-        "last_update": "0001-01-01T00:00:00",
-        "ignore_prerelease": "true",
-        "install_dir": "."
     }
 
 
@@ -78,6 +73,10 @@ class FileNotInZip(Exception):
     pass
 
 
+class HaltandRunScripts(Exception):
+    pass
+
+
 update_everything = False
 try:
     i = sys.argv.index("--update-everything")
@@ -92,13 +91,13 @@ class Updater:
         self.blockname = blockname
         self.repository = config.get(blockname, "repository")
         self._file = config.get(self.blockname, "file", fallback="mod.json")
+        self.ignore_updates = config.getboolean(blockname, "ignore_updates", fallback=False)
+        if self.ignore_updates:
+            print(f"[{time.strftime('%H:%M:%S')}] [info]    Search stopped for new releases  for {self.blockname}, ignore_updates flag is set")
+            return
+        self.ignore_prerelease = config.getboolean(blockname, "ignore_prerelease", fallback=True)
         self.repo = g.get_repo(self.repository)
-        self.ignore_prerelease = config.getboolean(
-            blockname, "ignore_prerelease", fallback=True
-        )
-        self.install_dir = Path(
-            config.get(blockname, "install_dir", fallback="./R2Northstar/mods")
-        )
+        self.install_dir = Path(config.get(blockname, "install_dir", fallback="./R2Northstar/mods"))
         self.file = (self.install_dir / self._file).resolve()
         self.exclude_files = config \
             .get(blockname, "exclude_files", fallback="") \
@@ -130,7 +129,7 @@ class Updater:
         raise NoValidRelease("Found No new releases")
 
     def asset(self, release: GitRelease) -> str:
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Updating to   new release  for {self.blockname} published Version {release.tag_name}")
+        print(f"[{time.strftime('%H:%M:%S')}] [info]    Updating to        new release   for {self.blockname} published Version {release.tag_name}")
         assets = release.get_assets()
 
         if assets.totalCount == 0:  # if no application release exists try download source direct.
@@ -190,21 +189,24 @@ class Updater:
             self._mod_json_extractor(zip_)
 
     def run(self):
+        if self.ignore_updates:
+            return
         try:
             release = self.release()
             url = self.asset(release)
         except NoValidRelease:
-            print(f"[{time.strftime('%H:%M:%S')}] [info]    Found no      new releases for {self.blockname}")
+            print(f"[{time.strftime('%H:%M:%S')}] [info]    Latest Version already installed for {self.blockname}")
             return
         except NoValidAsset:
-            print(f"[{time.strftime('%H:%M:%S')}] [warning] Possibly faulty   release  for {self.blockname} published Version {release.tag_name} has no vali assets")
+            print(
+                f"[{time.strftime('%H:%M:%S')}] [warning] Possibly faulty        release   for {self.blockname} published Version {release.tag_name} has no valit assets")
             return
         with tempfile.NamedTemporaryFile() as download_file:
             download(url, download_file)
             release_zip = zipfile.ZipFile(download_file)
             self.extract(release_zip)
             self.last_update = release.published_at
-            print(f"[{time.strftime('%H:%M:%S')}] [info]    Updated successfully           {self.blockname} to        Version {release.tag_name}")
+            print(f"[{time.strftime('%H:%M:%S')}] [info]    Installed successfully update    for {self.blockname} to        Version {release.tag_name}")
 
 
 class SelfUpdater(Updater):
@@ -216,7 +218,7 @@ class SelfUpdater(Updater):
             if update_everything or \
                     not self.file.exists() or \
                     release.published_at > self.last_update or \
-                    datetime.fromtimestamp(self.file.stat().st_mtime) < release.published_at - timedelta(hours=1):
+                    datetime.fromtimestamp(self.file.stat().st_mtime) < release.published_at - timedelta(minutes=10):
                 try:  # if asset not available contine search
                     return release, self.asset(release)
                 except NoValidAsset:
@@ -225,7 +227,7 @@ class SelfUpdater(Updater):
         raise NoValidRelease("No new release found")
 
     def asset(self, release: GitRelease):
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Updating to   new release  for {self.blockname} published Version {release.tag_name}")
+        print(f"[{time.strftime('%H:%M:%S')}] [info]    Updating to        new release   for {self.blockname} published Version {release.tag_name}")
         assets = release.get_assets()
         for asset in assets:
             if asset.content_type in "application/octet-stream":
@@ -233,39 +235,52 @@ class SelfUpdater(Updater):
         raise NoValidAsset("No valid asset was found in release")
 
     def run(self):
+        if self.ignore_updates:
+            return
         try:
             release, asset = self.release()
         except NoValidRelease:
-            print(f"[{time.strftime('%H:%M:%S')}] [info]    Found no      new releases for {self.blockname}")
+            print(f"[{time.strftime('%H:%M:%S')}] [info]    Latest Version already installed for {self.blockname}")
             return
         except NoValidAsset:
-            print(f"[{time.strftime('%H:%M:%S')}] [warning] Possibly faulty   release  for {self.blockname} published Version {release.tag_name} has no vali assets")
+            print(f"[{time.strftime('%H:%M:%S')}] [warning] Possibly faulty        release   for {self.blockname} published Version {release.tag_name} has no valit assets")
             return
         with tempfile.NamedTemporaryFile(delete=False) as download_file:
             download(asset.browser_download_url, download_file)
 
         newfile: Path = self.file.with_suffix(".new")
         shutil.move(download_file.name, newfile)
-        script = f"timeout 20 && del {self.file} && move {newfile} {self.file} && del {newfile}"
-        subprocess.Popen(["cmd", "/c", script])
+        # print(f"[{time.strftime('%H:%M:%S')}] [info]    Running self-replacer            for {self.blockname} to        Version {release.tag_name}")
         self.last_update = release.published_at
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Updated successfully           {self.blockname} to        Version {release.tag_name}")
+        print(f"[{time.strftime('%H:%M:%S')}] [info]    Stopped Updater and rerun new Version of {self.blockname} after install")
+
+        global script_queue
+        script_queue.append(
+            f"echo [{time.strftime('%H:%M:%S')}] [info]    Running self-replacer            for {self.blockname} to        Version {release.tag_name} &&"
+            f"timeout /t 5 && del {self.file} && move {newfile} {self.file} &&"
+            f"echo [{time.strftime('%H:%M:%S')}] [info]    Installed successfully update    for {self.blockname} to        Version {release.tag_name} &&"
+            f"echo [{time.strftime('%H:%M:%S')}] [info]    Launching latest install         of  {self.file.name} &&"
+            f"{self.file}"
+        )
+        raise HaltandRunScripts("restart manager")
 
 
 def main():
-
-    while not updater():  # restart for github rate error
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Waiting and restarting Updater in 60s...")
-        time.sleep(60)
-
-    launcher()
+    try:
+        while not updater():  # restart for github rate error
+            print(f"[{time.strftime('%H:%M:%S')}] [info]    Waiting and restarting Updater in 60s...")
+            time.sleep(60)
+        launcher()
+    except HaltandRunScripts:
+        for script in script_queue:
+            subprocess.Popen(script, cwd=str(Path.cwd()), shell=True)
 
 
 def updater() -> bool:
     for section in config.sections():
         try:
             if section not in ("Launcher", "ExampleMod"):
-                print(f"[{time.strftime('%H:%M:%S')}] [info]    Searching for new Releases for {section}...")
+                print(f"[{time.strftime('%H:%M:%S')}] [info]    Searching for      new releases  for {section}...")
                 if section == "NorthstarManager":
                     u = SelfUpdater(section)
                     u.run()
@@ -273,8 +288,9 @@ def updater() -> bool:
                     u = Updater(section)
                     u.run()
         except RateLimitExceededException:
-            print(f"[{time.strftime('%H:%M:%S')}] [warning] GitHub rate exceeded for {section}.")
-            print(f"[{time.strftime('%H:%M:%S')}] [info]    Available requests left {g.rate_limiting[0]}/{g.rate_limiting[1]}.")
+            print(f"[{time.strftime('%H:%M:%S')}] [warning] GitHub rate exceeded for {section}")
+            print(
+                f"[{time.strftime('%H:%M:%S')}] [info]    Available requests left {g.rate_limiting[0]}/{g.rate_limiting[1]}")
             inp = input("Launch Northstar without checking for updates? (y/n) ")
             if inp != "n":
                 break
@@ -286,22 +302,16 @@ def updater() -> bool:
 
 def launcher():
     try:
-        action = "./__Installer/Origin/redist/internal/OriginThinSetup.exe"
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Launching OriginThinClient")
-        subprocess.run([action], cwd=str(Path.cwd()))
-        print(f"[{time.strftime('%H:%M:%S')}] [info]    Launching {config.get('Launcher', 'filename')} "
-              f"{config.get('Launcher', 'arguments')} "
-              f"{''.join(sys.argv[1:])} in 10sec...")
+        script = "__Installer/Origin/redist/internal/OriginThinSetup.exe"
+        print(f"[{time.strftime('%H:%M:%S')}] [info]    Launching and waiting (10sec) for OriginThinClient")
+        subprocess.run([script], cwd=str(Path.cwd()))
         time.sleep(10)
-        action = config.get('Launcher', 'filename')
-        subprocess.run(
-            [action]
-            + config.get("Launcher", "arguments").split(" ")
-            + sys.argv[1:],
-            cwd=str(Path.cwd())
-        )
+
+        script = [config.get('Launcher', 'filename')] + config.get('Launcher', 'arguments').split(" ") + sys.argv[1:]
+        print(f"[{time.strftime('%H:%M:%S')}] [info]    Launching {' '.join(script)}")
+        subprocess.run(script, cwd=str(Path.cwd()))
     except FileNotFoundError:
-        print(f"[{time.strftime('%H:%M:%S')}] [warning] Could not run {action}")
+        print(f"[{time.strftime('%H:%M:%S')}] [warning] Could not run {script}")
 
 
 main()
