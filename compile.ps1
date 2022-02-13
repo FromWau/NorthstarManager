2a2085930e43ef67c70e296159f5f6ccb67c1911
Frommhund <56253401+Frommhund@users.noreply.github.com>
2022-02-13 01:19:46 +0100

    switched to yaml configuration

--- compile.ps1
+++ compile.ps1
@@ -1,4 +1,3 @@
 python -m pip install --upgrade pip
-python -m pip install tqdm confuse requests pygithub nuitka
+python -m pip install tqdm confuse requests pygithub pyaml nuitka
 python -m nuitka --standalone --onefile --python-flag=-O --enable-plugin=anti-bloat --clang --include-module=tqdm,confuse,requests --follow-imports --assume-yes-for-downloads --windows-icon-from-ico=ns_icon_pink.ico NorthstarManager.py
-
