3081c2ff51fa9bdf148f9b5d444b824695d8112a
Frommhund <56253401+Frommhund@users.noreply.github.com>
2022-02-13 01:39:53 +0100

    updated compile.ps1

--- compile.ps1
+++ compile.ps1
@@ -1,3 +1,5 @@
-python -m pip install --upgrade pip
+$s=Get-Date 
+python -m pip install --upgrade pip 
 python -m pip install tqdm confuse requests pygithub pyaml nuitka
 python -m nuitka --standalone --onefile --python-flag=-O --enable-plugin=anti-bloat --clang --include-module=tqdm,confuse,requests --follow-imports --assume-yes-for-downloads --windows-icon-from-ico=ns_icon_pink.ico NorthstarManager.py
+$e=Get-Date; Write-Host "Compiling NorthstarManager.exe took "($e - $s).TotalSeconds" seconds"
